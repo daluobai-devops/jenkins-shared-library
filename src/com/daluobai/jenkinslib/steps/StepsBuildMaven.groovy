28777a0641fe356f25b2b2ce4abce9e4eb1ce511
wuzhaozhongguo <wuzhaozhongguo@hotmail.com>
2023-03-01 23:39:54 +0800

    切换 settings.xml

--- src/com/daluobai/jenkinslib/steps/StepsBuildMaven.groovy
+++ src/com/daluobai/jenkinslib/steps/StepsBuildMaven.groovy
@@ -69,8 +69,6 @@ class StepsBuildMaven implements Serializable {
                 steps.sh """
                         rm -f /root/.m2/settings.xml
                         touch /root/.m2/settings.xml
-                        """
-                steps.sh """
                         cat > /root/.m2/settings.xml <<-'EOF' ${settingsXmlStr} EOF
                         """
                 steps.sh """
