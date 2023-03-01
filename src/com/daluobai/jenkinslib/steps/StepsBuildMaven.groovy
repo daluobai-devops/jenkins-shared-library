2835686d9d77240a0cc0ba8972f074e19a555679
wuzhaozhongguo <wuzhaozhongguo@hotmail.com>
2023-03-01 21:56:39 +0800

    切换 settings.xml

--- src/com/daluobai/jenkinslib/steps/StepsBuildMaven.groovy
+++ src/com/daluobai/jenkinslib/steps/StepsBuildMaven.groovy
@@ -69,7 +69,7 @@ class StepsBuildMaven implements Serializable {
                 steps.sh """
                         #! /bin/bash -eu
                         set -eo pipefail
-                        cat > /root/.m2/settings.xml << EOF "11111" EOF
+                        cat > /root/.m2/settings.xml << EOF 11111 EOF
                         mkdir -p ${pathBase}/${pathPackage} && mkdir -p ${pathBase}/${pathCode}
                         cd ${pathBase}/${pathCode}
                         git config --global http.version HTTP/1.1
