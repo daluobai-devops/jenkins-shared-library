7ac9db61581659037e1d8fd6ca35ab5fc8413248
wuzhao <wuzhaozhongguo@hotmail.com>
2023-07-07 15:34:08 +0800

    增加探针支持

--- src/com/daluobai/jenkinslib/utils/EndpointUtils.groovy
+++ src/com/daluobai/jenkinslib/utils/EndpointUtils.groovy
@@ -100,6 +100,7 @@ class EndpointUtils implements Serializable {
         for (int i = 0; i < failureThreshold; i++) {
             steps.echo "健康检查-第${i}次"
             sleep period
+            //加上wc -l会导致结果不对，所以按照是否有返回值判断
             def portListeningStr = steps.sh returnStdout: true, script: """netstat -an | egrep '^.*9000\\s' | awk '\$1 ~ /tcp/ && \$6 == "LISTEN" {print \$0}'"""
 
             boolean portListening = ObjectUtil.isNotEmpty(portListeningStr) && ObjectUtil.isNotEmpty(portListeningStr.trim())
