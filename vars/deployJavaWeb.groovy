d08646665de7b1ef09a30a2bd9f5b49fab3587e6
wuzhao <wuzhaozhongguo@hotmail.com>
2023-07-07 17:40:53 +0800

    增加探针支持

--- vars/deployJavaWeb.groovy
+++ vars/deployJavaWeb.groovy
@@ -87,7 +87,7 @@ def call(Map customConfig) {
                     if (currentBuild.currentResult == "SUCCESS"){
                         messageContent = "构建成功: ${currentBuild.fullDisplayName}"
                     }else{
-                        messageContent = "构建完成: ${currentBuild.fullDisplayName},异常信息: ${errMessage}"
+                        messageContent = "构建失败: ${currentBuild.fullDisplayName},异常信息: ${errMessage}"
                     }
                     wecomApi.sendMsg(customConfig.SHARE_PARAM.message.wecom.key, messageContent)
                 }
