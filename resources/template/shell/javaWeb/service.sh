5550ad7b461cd94f8b814d8c55bb52d33557aaf0
wuzhao <wuzhaozhongguo@hotmail.com>
2023-07-07 17:49:41 +0800

    增加探针支持

--- resources/template/shell/javaWeb/service.sh
+++ resources/template/shell/javaWeb/service.sh
@@ -1,80 +1,61 @@
 #!/bin/bash
 
-# 定义java服务的路径和名称
-SHELL_PATH=\$(readlink -f "\$0")
-SHELL_DIR=\$(dirname "\$path")
-SERVICE_DIR=\$SHELL_DIR
-SERVICE_NAME=${appName}
-
-# 定义pid文件的路径和名称
-PID_FILE=\$SERVICE_DIR/service.pid
-
-# 定义启动、停止和重启的函数
+# Start the java project
 start() {
-  # 检查pid文件是否存在
-  if [ -f \$PID_FILE ]; then
-    # 读取pid文件中的进程号
-    pid=\$(cat \$PID_FILE)
-    # 检查进程是否存在
-    if kill -0 \$pid >/dev/null 2>&1; then
-      echo "\$SERVICE_NAME is already running (pid: \$pid)"
-      return
-    else
-      # 如果进程不存在，删除pid文件
-      rm -f \$PID_FILE
-    fi
+  # Check if the java process is running
+  PID=\$(ps -ef | grep "-Dapp.name=${appName}" | grep -v grep | awk '{print \$2}')
+  if [ -n "\$PID" ]; then
+    # The process is already running, do nothing
+    echo "The app is already running, pid: \$PID"
+    return 0
   fi
-  # 启动服务，并将进程号写入pid文件
-  echo "Starting \$SERVICE_NAME ..."
+  # Start the java project in the background and redirect the output to a log file
   nohup ${javaPath} -Dapp.name=${appName} ${runOptions} -jar ${archiveName} ${runArgs}  >/dev/null 2>&1 &
-  echo \$! > \$PID_FILE
-  echo "\$SERVICE_NAME started"
+  # Get the pid of the java process
+  PID=\$!
+  # Echo the pid to the console
+  echo "The app is started, pid: \$PID"
 }
 
+# Stop the java project
 stop() {
-  # 检查pid文件是否存在
-  if [ -f \$PID_FILE ]; then
-    # 读取pid文件中的进程号
-    pid=\$(cat \$PID_FILE)
-    # 检查进程是否存在
-    if kill -0 \$pid >/dev/null 2>&1; then
-      # 停止服务，并删除pid文件
-      echo "Stopping \$SERVICE_NAME ..."
-      kill -9 \$pid
-      rm -f \$PID_FILE
-      echo "\$SERVICE_NAME stopped"
-    else
-      # 如果进程不存在，删除pid文件，并提示服务没有运行
-      rm -f \$PID_FILE
-      echo "\$SERVICE_NAME is not running"
-    fi
-  else
-    # 如果pid文件不存在，提示服务没有运行
-    echo "\$SERVICE_NAME is not running"
+  # Check if the java process is running
+  PID=\$(ps -ef | grep "-Dapp.name=${appName}" | grep -v grep | awk '{print \$2}')
+  if [ -n "\$PID" ]; then
+    # Kill the process with SIGTERM signal
+    kill \$PID
+    # Wait for the process to exit
+    wait \$PID
+    # Echo a message to the console
+    echo "The app is stopped, pid: \$PID"
+    return 0
   fi
+  # The process is not running, do nothing
+  echo "The app is not running"
 }
 
+# Restart the java project
 restart() {
-  # 重启服务，相当于先停止再启动
+  # Stop the java project first
   stop
+  # Start the java project again
   start
 }
 
-# 根据传入的参数执行相应的函数
+# Check the command line argument and call the corresponding function
 case "\$1" in
-  start)
-    start
-    ;;
-  stop)
-    stop
-    ;;
-  restart)
-    restart
-    ;;
-  *)
-    # 如果没有传入参数或者参数不合法，打印帮助信息
-    echo "Usage: \$0 {start|stop|restart}"
+start)
+start
+;;
+stop)
+stop
+;;
+restart)
+restart
+;;
+*)
+echo "Usage: \$0 {start|stop|restart}"
+exit 1
+;;
 esac
-
-exit 0
 
