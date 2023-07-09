6eb510f4f743bc9c02d07833eb4d12d4a23e23c4
wuzhao <wuzhaozhongguo@hotmail.com>
2023-07-09 22:58:25 +0800

    增加探针支持

---
+++ src/com/daluobai/jenkinslib/steps/Test.groovy
@@ -0,0 +1,15 @@
+package com.daluobai.jenkinslib.steps
+
+/**
+ * @title Test
+ * @description <TODO description class purpose>
+ * @author wuzhao
+ *  version 1.0.0
+ * @create 2023/7/9 22:55
+ */
+class Test implements ITest {
+    @Override
+    def call() {
+        print("test111111")
+    }
+}
