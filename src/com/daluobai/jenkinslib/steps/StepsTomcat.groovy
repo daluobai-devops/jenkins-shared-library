ef4f1ebcb6633eab9ccb307d075b529424c42fa9
wuzhao <wuzhaozhongguo@hotmail.com>
2023-07-11 15:41:31 +0800

    增加探针支持

--- src/com/daluobai/jenkinslib/steps/StepsTomcat.groovy
+++ src/com/daluobai/jenkinslib/steps/StepsTomcat.groovy
@@ -32,8 +32,8 @@ class StepsTomcat implements Serializable {
     def test() {
         def classLoader = this.class.classLoader
         def contentEntryClass = classLoader.loadClass ("com.daluobai.jenkinslib.steps.Test")
-        def constructor = contentEntryClass.getConstructor (this.steps.getClass ())
-        def contentEntry = constructor.newInstance (this.steps)
+        def constructor = contentEntryClass.getConstructor(this.steps.getClass ())
+        def contentEntry = constructor.newInstance(this.steps)
 
         contentEntry.call()
         steps.echo "StepsTomcat:test"
