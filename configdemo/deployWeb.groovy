bc8069373bbe31c1ab4049b6cae7e8a799cd113d
wuzhao <wuzhaozhongguo@hotmail.com>
2023-07-13 23:00:41 +0800

    1

--- configdemo/deployWeb.groovy
+++ configdemo/deployWeb.groovy
@@ -24,7 +24,7 @@ def customConfig = [
                         "gitBranch": "master",
                         //构建命令 必填
                         "buildCMD"   : "npm install && npm run build",
-                        //用来打包的镜像 可选
+                        //用来打包的镜像 可选。默认10.16.0(10.16.0,14.21.3,16)
                         "dockerBuildImage"   : "registry.cn-hangzhou.aliyuncs.com/wuzhaozhongguo/build-npm:10.16.0",
                         //使用缓存node_modules 可选，默认true
                         "cacheNodeModules": "true"
