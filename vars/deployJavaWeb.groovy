2b4b182400762addfe671d4e0ac431ebe31435c5
wuzhao <wuzhaozhongguo@hotmail.com>
2023-07-13 23:32:42 +0800

    1

--- vars/deployJavaWeb.groovy
+++ vars/deployJavaWeb.groovy
@@ -142,7 +142,7 @@ def mergeConfig(Map customConfig) {
     }
     echo "fullConfigParams2: ${fullConfigParams.toString()}"
     echo "fullConfigParams3: ${fullConfigParams.root().getProperties().toMapString()}"
-    fullConfig = fullConfigParams.root().unwrapped();
+    fullConfig = fullConfigParams.root().getProperties();
 
     return MapUtils.deepCopy(fullConfig)
 }
