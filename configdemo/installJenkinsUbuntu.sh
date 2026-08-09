489fe0e43a218dd273ec20953a767b5c1858150f
wuzhaozhongguo <wuzhaozhongguo@hotmail.com>
2026-08-09 20:46:14 +0800

    Update Jenkins key URL in installation script

--- configdemo/installJenkinsUbuntu.sh
+++ configdemo/installJenkinsUbuntu.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install fontconfig openjdk-21-jre -y
 sudo java -version
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
